081fcd75fe06e8955f53b4e08c7b0c4df396a098
owais-js <owaisshabbir3131@gmail.com>
2026-07-09 10:21:21 +0500

    add staff routers in main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from app.api.patient import router as patient_router
 from app.api.doctor import router as doctor_router
+from app.api.staff import router as staff_router
 
 app = FastAPI(
     title="Hospital Management API"
@@ -9,6 +10,7 @@ app = FastAPI(
 
 app.include_router(patient_router)
 app.include_router(doctor_router)
+app.include_router(staff_router)
 
 @app.get("/")
 def home():
